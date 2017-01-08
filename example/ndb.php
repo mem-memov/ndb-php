211b075ce7696c1a82eedeaf26cea2e877c48157
Alexander Novikov <mem-memov@yandex.ru>
2017-01-08 06:35:31 +0300

    example

--- example/ndb.php
+++ example/ndb.php
@@ -12,24 +12,28 @@ for ($i = 0; $i < $length; $i++) {
     $ids[] = $id;
 }
 
-foreach ($ids as $id_1) {
-    foreach ($ids as $id_2) {
-        if ($id_1 !== $id_2) {
-            $client->connect($id_1, $id_2);
-            $client->connect($id_2, $id_1);
-        }
-    }
+print_r("create\n");
+print_r($ids);
+
+$client->connect($ids[0], $ids[1]);
+$client->connect($ids[0], $ids[2]);
+$client->connect($ids[1], $ids[2]);
+$client->connect($ids[2], $ids[1]);
+
+print_r("read\n");
+foreach ($ids as $id) {
+    $connections = $client->read($id);
+    print_r($connections);
 }
 
 $intersect = $client->intersect([$ids[0], $ids[2]]);
-$union = $client->union([$ids[0], $ids[2]]);
-$difference = $client->difference([$ids[0], $ids[2]]);
-
-print_r("create");
-print_r($ids);
-print_r("intersect");
+print_r("intersect\n");
 print_r($intersect);
-print_r("union");
+
+$union = $client->union([$ids[0], $ids[2]]);
+print_r("union\n");
 print_r($union);
-print_r("difference");
+
+$difference = $client->difference([$ids[0], $ids[2]]);
+print_r("difference\n");
 print_r($difference);
